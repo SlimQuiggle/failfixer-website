c2d9c0cb50021072d34bc18faf85fff81ca4b4e9
Slim Flexxxington <ilovetxva@gmail.com>
2026-02-26 20:54:28 -0600

    Wire Lemon Squeezy checkout link

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 // FailFixer Website Configuration
 // Replace this URL with your actual Gumroad product link
-window.FAILFIXER_GUMROAD_URL = '';
+window.FAILFIXER_GUMROAD_URL = 'https://failfixer.lemonsqueezy.com/checkout/buy/02db0c68-ad77-49bc-af20-276414cd3d57';
 // Example: window.FAILFIXER_GUMROAD_URL = 'https://yourname.gumroad.com/l/failfixer';
